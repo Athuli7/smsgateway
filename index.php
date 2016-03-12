2ce936e8b756e727fa9cc429db608fc15666b57d
Athul Raj <athulbraj@gmail.com>
2016-03-12 15:17:32 +0530

    Removed passwords

---
+++ index.php
@@ -0,0 +1,25 @@
+<form method="post">
+	<input name="number" placeholder = "9020508050">
+	<br>
+	<input type="text" name="message" placeholder = "Hey there, just testing ! Avoid punctuations of all kinds ! ">
+	<br>
+	<input type="submit" value="Submit">
+</form>
+
+<?php
+
+include "smsGateway.php";	
+if(isset($_POST['number'])){
+	$smsGateway = new SmsGateway('smsgateway.me@athulraj.com', 'ScaleXYPubPass');
+	$deviceID = 19195;
+	$number = '+91'.$_POST['number'];
+	$message = $_POST['message'];
+	$options = [
+	//'send_at' => strtoti1me('+10 minutes'), // Send the message in 10 minutes
+	'expires_at' => strtotime('+1 hour') // Cancel the message in 1 hour if the message is not yet sent
+	];
+	echo "log :- <br>";
+	var_dump( $smsGateway->sendMessageToNumber($number, $message, $deviceID, $options) );
+}
+
+?>
